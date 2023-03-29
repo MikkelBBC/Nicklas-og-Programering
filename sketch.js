6bd33723f33032fa19b69f8c5c86b3fdffa211d5
MikkelBBC <129258724+MikkelBBC@users.noreply.github.com>
2023-03-29 09:37:35 +0100

    Update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+let billede = ["0.png","1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png","11.png"];
+let LavBillede;
+
 function setup() {
     createCanvas(400, 400);
-  }
+    LavBillede = createImg(billede,"hej");
+}
   
   function draw() {
     background(220);
-  }
+    rect(10,10,10,10);
+    
+}

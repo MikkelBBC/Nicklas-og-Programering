14e1dcca649f6f86f0f79c697f129b109590a595
RedNickster <n.stenlokke@gmail.com>
2023-04-08 14:51:46 +0200

    Captions på skærm
    Jeg har bare gjordt klar så vi kan finde på captions næste gang vi ses.  Har også lavet texten i en anden font som vi bare kan ændre, og ændre textstørrelsen.

--- sketch.js
+++ sketch.js
@@ -1,25 +1,40 @@
 
 let billeder = [];
+let captions = ["caption1", 
+                "caption2", 
+                "caption3", 
+                "caption4", 
+                "caption5", 
+                "caption6", 
+                "caption7", 
+                "caption8", 
+                "caption9", 
+                "caption10", 
+                "caption11", 
+                "caption12"];
+let vælgBillede;
+let vælgCaption;
+
 
 function preload() {
   for (let i = 0; i < 12; i++) {
     billeder[i] = loadImage("Billder/" + i + ".png");
   }
 
-  // load all images
+ vælgCaption = floor(random(0, 11));
  vælgBillede = floor(random(0, 11));
 }
 
 function setup() {
     createCanvas(400, 400);
   
-
   }
-  
+
   function draw() {
     background(255);
-    rect(10,10,10,10);
 
-    image(billeder[vælgBillede],130,50,300,300);
-    
+    image(billeder[vælgBillede],50,100,350,300);
+    textSize(18);
+    textFont("Trebuchet MS");
+    text(captions[vælgCaption], 40, 15, 350);
 }

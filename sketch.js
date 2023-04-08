23defb2c8126fef99b05de04c9d138787f7cb943
RedNickster <n.stenlokke@gmail.com>
2023-04-08 16:04:57 +0200

    Knapper
    Jeg har lavet en knap der laver et nyt meme hver gang man trykker på knappen.
    Jeg har også gjordt klar til at vi kan implementere like/dislike systemet.

--- sketch.js
+++ sketch.js
@@ -14,7 +14,9 @@ let captions = ["caption1",
                 "caption12"];
 let vælgBillede;
 let vælgCaption;
-
+let nyMeme;
+let like;
+let dislike;
 
 function preload() {
   for (let i = 0; i < 12; i++) {
@@ -27,7 +29,17 @@ function preload() {
 
 function setup() {
     createCanvas(400, 400);
-  
+    nyMeme = createButton("Ny Meme");
+    nyMeme.position(345, 30);
+    nyMeme.mousePressed(Restart);
+
+    like = createButton("Like");
+    like.position(345, 60);
+    like.mousePressed(Like);
+    
+    dislike = createButton("Dislike");
+    dislike.position(345, 90);
+    dislike.mousePressed(Dislike);
   }
 
   function draw() {
@@ -37,4 +49,20 @@ function setup() {
     textSize(18);
     textFont("Trebuchet MS");
     text(captions[vælgCaption], 40, 15, 350);
+    
+}
+
+function Restart() {
+  vælgCaption = floor(random(0, 11));
+  vælgBillede = floor(random(0, 11));
+}
+
+function Like() {
+  console.log("Like");
+  // give current picture and caption a like
+}
+
+function Dislike() {
+  console.log("Dislike");
+  // give current picture and caption a dislike
 }
